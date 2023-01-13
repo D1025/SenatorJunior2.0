3b9482fb7744562a5299ad9457664c5dd49162e3
Donald <patryk5531@gmail.com>
2023-01-13 23:13:42 +0100

    some minor changes

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import time
 import hikari
 import lightbulb
 import miru
-import gates
-import race
 import dungeon
 import roller
 
@@ -17,26 +15,6 @@ async def on_started(event):
     print("Bot has started!")
     
     
-@bot.command
-@lightbulb.option("trudnosc", "Trudnosc trasy", type=int, required=True)
-@lightbulb.command('race', 'Sprawdzmy sie w wyscigu')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def test(ctx):
-    NewEmbed = race.EmbedTournament(ctx.options.trudnosc)
-    view = race.ButtonView(NEmbed=NewEmbed, timeout=3600)
-    message = await ctx.respond(NewEmbed.callBackEmbed(), components=view.build())
-    await view.start(message)
-    
-    
-@bot.command
-@lightbulb.option("enemy", "Z kim chcesz grac?", type=hikari.User, required=True)
-@lightbulb.command("gates", "zagrajmy w bramy")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def gates(ctx):
-    gameGates = gates.Gates(ctx.author, ctx.options.enemy)
-    view = gates.ButtonViewGates(NGates=gameGates, timeout=600)
-    message = await ctx.respond(gameGates.callBackEmbed(), components=view.build())
-    await view.start(message)
     
 @bot.command
 @lightbulb.command('dnd', 'Dungeons and Dragons 5e')
@@ -56,7 +34,7 @@ async def dnd(ctx):
 @lightbulb.command('make', 'Makes random character for dnd 5e')
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def make(ctx):
-    Character = dungeon.DungeonsAndDragons(ctx.options.intelligence, ctx.author.id)
+    Character = dungeon.DungeonsAndDragons(inteligence=ctx.options.intelligence, author=ctx.author.id)
     dungeonView = dungeon.ButtonViewDungeon(NDungeon=Character, timeout=120)
     name = "".join(["Arts/Stats/",str(ctx.author.id), ".png"])
     message = await ctx.respond(Character.ReturnEmbed(ctx,name), components=dungeonView.build())
@@ -79,7 +57,7 @@ async def show(ctx):
 @lightbulb.command('show', 'test')
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def nime(ctx):
-    Character = dungeon.DungeonsAndDragons(author=ctx.author.id, id=ctx.options.id)
+    Character = dungeon.DungeonsAndDragons.from_file(author=ctx.author.id, id=ctx.options.id)
     dungeonView = dungeon.ButtonViewDungeon(NDungeon=Character, timeout=120)
     name = "".join(["Arts/Stats/",str(ctx.author.id), ".png"])
     message = await ctx.respond(Character.ReturnEmbed(ctx,name), components=dungeonView.build())
@@ -108,4 +86,4 @@ async def roll(ctx):
    
     
 time.sleep(1)
-bot.run(status=hikari.Status.DO_NOT_DISTURB, activity=hikari.Activity(name="DEVELOPMENT", type=hikari.ActivityType.PLAYING, url="https://discord.com/api/oauth2/authorize?client_id=1058750318181105674&permissions=543853770816&scope=bot"))
+bot.run(status=hikari.Status.ONLINE, activity=hikari.Activity(name="version 2.0 baby", type=hikari.ActivityType.PLAYING, url="https://discord.com/api/oauth2/authorize?client_id=818488309353283624&permissions=414464728128&scope=bot"))
